d5faeb53ae2b4b351b2a28d52ba404713162a5bf
rickymsmith <35548840+rickymsmith@users.noreply.github.com>
2022-04-12 20:40:01 -0500

    cycling colors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 const myBox = document.getElementById('my-box');
+// var colorInterval = setInterval(setColor, 1000);
 
-
-function setColor(){
-if (myBox.style.backgroundColor == 'red'){
-    myBox.style.backgroundColor = 'blue';} else {
-        myBox.style.backgroundColor = 'red'
-    }
-  
-}
+function changeColor(){
+if (myBox.style.backgroundColor === 'red'){
+    myBox.style.backgroundColor = 'blue';}
+    else if (myBox.style.backgroundColor === 'blue'){ 
+        myBox.style.backgroundColor = 'yellow';
+    } else {
+        myBox.style.backgroundColor = 'red';
+    }}
 
 function onClick(){
-    setColor();
+    setInterval(changeColor, 1000);
 
 }
 
